0ff82da59c18652009542f0c9da26979a6fce6cf
2xiao <827574297wxxtb@sina.com>
2024-07-04 05:09:13 -0400

    update vue book

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -21,28 +21,23 @@ export default sidebar({
 					activeMatch: '^/leetcode/outline/tag'
 				}
 			],
-			collapsible: true,
-			expanded: true
+			collapsible: true
 		},
 		{
 			text: '第二章 数据结构',
 			prefix: 'ds/',
 			children: 'structure',
-			collapsible: true,
-			expanded: true
+			collapsible: true
 		},
 		{
 			text: '第三章 算法',
 			prefix: 'algorithm/',
 			children: 'structure',
-			collapsible: true,
-			expanded: true
+			collapsible: true
 		},
 		{
 			text: '第四章 LeetCode 题解',
 			prefix: 'problem/',
-			collapsible: true,
-			expanded: true,
 			children: [
 				// AUTO_GEN_CONFIG_START
 				{
@@ -431,13 +426,38 @@ export default sidebar({
 	'/vue/': [
 		'',
 		{
-			text: 'Vue3 整体架构',
+			text: '整体架构',
 			children: ['1', '2'],
 			collapsible: false
 		},
 		{
-			text: 'Vue3 响应式原理',
+			text: '响应式原理',
 			children: ['3', '4', '5', '6', '7'],
+			collapsible: false
+		},
+		{
+			text: '渲染原理',
+			children: ['8', '9'],
+			collapsible: false
+		},
+		{
+			text: 'Diff 算法原理',
+			children: ['10'],
+			collapsible: false
+		},
+		{
+			text: '组件渲染原理',
+			children: ['11', '12', '13', '14', '15', '16'],
+			collapsible: false
+		},
+		{
+			text: '其他实现原理',
+			children: ['17', '18', '19', '20'],
+			collapsible: false
+		},
+		{
+			text: '模板编译原理',
+			children: ['21', '22', '23', '24'],
 			collapsible: false
 		}
 	],
